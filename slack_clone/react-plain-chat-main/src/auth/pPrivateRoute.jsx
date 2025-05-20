0f1635e09c575acc5e37541082130f2eb3950fa0
Farhan Basha <farhanbashastays@gmail.com>
2025-05-20 22:14:52 +0530

    file has been deleted

--- slack_clone/react-plain-chat-main/src/auth/pPrivateRoute.jsx
+++
@@ -1,9 +0,0 @@
-import { useAuth } from './authContext';
-import { Navigate, Outlet } from 'react-router-dom';
-
-const PrivateRoute = () => {
-  const { isAuthenticated } = useAuth();
-  return isAuthenticated ? <Outlet /> : <Navigate to="/login" />;
-};
-
-export default PrivateRoute;
